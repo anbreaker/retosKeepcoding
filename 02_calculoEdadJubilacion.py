324dff3633be4af3175b09968f79306b67d8fd9e
anbreaker <antunez19@gmail.com>
2019-10-22 21:21:37 +0200

    empty now

---
+++ 02_calculoEdadJubilacion.py
@@ -0,0 +1,12 @@
+# Cálculo de la jubilación
+# Incorpora el año actual al programa. Crea un programa que cuente cuantos años faltan para tu jubilación y que te diga el año en que te jubilarás. Algo así
+
+#     ¿Cuantos años tienes? 48
+#     ¿A qué edad te jubilarás? 67
+#     Te quedan 19 años para jubilarte
+#     Estamos en 2018, te jubilarás en 2037.
+# Restricciones
+#     Convertir las cadenas de entrada en números
+#     Obten el año actual como lo haga python (no vale ponerlo como constante en el programa)
+# Reto
+#     Maneja la situación si el programa devuelve un número negativo de modo que diga que ya puede jubilarse
