189dcea8b2dd260e6ab2229b336b5cd3d7345591
anbreaker <antunez19@gmail.com>
2019-11-11 20:26:21 +0100

    numbers 9 does not work

--- 09_numerosRomanos.py
+++ 09_numerosRomanos.py
@@ -3,20 +3,27 @@
 # L	50	|   C	100	|   D	500 | M	1000
 
 numRomanos2 = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M'}
-
 numRomanos = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
 
 
 def descomponiendoNumArabigo(numero):  # Convertir a romano:
     resultado = ''
     for item in numRomanos:
-        resto = numero // numRomanos.get(item)
-        if resto > 0:
-            numero = numero - numRomanos.get(item)*resto
-            resultado += item*resto
+        cociente = numero // numRomanos.get(item)
+        if cociente > 0:
+            numero = numero - numRomanos.get(item)*cociente
+            mayorTres = item*cociente
+            if mayorTres == 'CCCC':
+                resultado += 'CD'
+            elif mayorTres == 'XXXX':
+                resultado += 'XL'
+            elif mayorTres == 'IIII':
+                resultado += 'IV'
+            else:
+                resultado += item*cociente
             print(
-                f'Letra Romana {item}\'s--> {resto}\t num Arabe--> {numRomanos.get(item)}')
-    print('Resultado al final --> ', resultado)
+                f'Letra Romana {item}\'s--> {cociente}\t num Arabe--> {numRomanos.get(item)}')
+    print(f'Resultado al final --> {resultado}')
     return resultado
 
 
@@ -27,7 +34,6 @@ def numToRoman(numero):
         for i in range(numero):
             resultado += numRomanos2.get(1)
     return resultado
-
 
 def validarNumero(numero):
     if numero > 0:
@@ -49,4 +55,7 @@ while not validarNumero(numero):
 
 # print(f'El numero {numero} en romano es: {numToRoman(numero)}')
 # print(f'El numero {numero} en romano es: {descomponiendoNumArabigo(234)}')
-print(f'El numero "1675" en romano es: {descomponiendoNumArabigo(1675)}')
+
+numero = 3587
+
+print(f'El numero {numero} en romano es: {descomponiendoNumArabigo(numero)}')
