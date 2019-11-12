404c124447c02fb037263f77cce7ec1302f2129c
anbreaker <antunez19@gmail.com>
2019-11-12 17:36:44 +0100

    Functional converter Roman Numbers

--- 09_numerosRomanos.py
+++ 09_numerosRomanos.py
@@ -3,7 +3,8 @@
 # L	50	|   C	100	|   D	500 | M	1000
 
 numRomanos2 = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M'}
-numRomanos = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+numRomanos = {'M': 1000, 'CM': 900, 'D': 500, 'C': 100,
+              'XC': 90, 'L': 50, 'X': 10, 'IX': 9, 'V': 5, 'I': 1}
 
 
 def descomponiendoNumArabigo(numero):  # Convertir a romano:
@@ -27,14 +28,6 @@ def descomponiendoNumArabigo(numero):  # Convertir a romano:
     return resultado
 
 
-'''
-def numToRoman(numero):
-    resultado = ''
-    if numero < 4:
-        for i in range(numero):
-            resultado += numRomanos2.get(1)
-    return resultado
-
 def validarNumero(numero):
     if numero > 0:
         # print(f'en la def {numero}')
@@ -51,11 +44,8 @@ while not validarNumero(numero):
             numero = int(input('Introduzca un numero entero: '))
     except ValueError:
         print('Debes introducir sólo numéros enteros positivos. ')
-'''
 
-# print(f'El numero {numero} en romano es: {numToRoman(numero)}')
-# print(f'El numero {numero} en romano es: {descomponiendoNumArabigo(234)}')
 
-numero = 3587
+# numero = 3999
 
 print(f'El numero {numero} en romano es: {descomponiendoNumArabigo(numero)}')
