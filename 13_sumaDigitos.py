fe29771b3ef8ba7f302d88d7c7522fea26d8ac78
anbreaker <antunez19@gmail.com>
2019-12-11 18:45:15 +0100

    Sumando digitos y formateado ok

--- 13_sumaDigitos.py
+++ 13_sumaDigitos.py
@@ -1,23 +1,37 @@
-Se trata de implementar un programa que sume los dígitos de un número entero no negativo.
-Por ejemplo, la suma de los dígitos del 3433 es 13.
+# Se trata de implementar un programa que sume los dígitos de un número entero no negativo.
+#           Ejemplo, la suma de los dígitos del 3433 es 13.
 
-Para darle un poco más de emoción, el programa no se limitará a escribir el resultado de
-la suma, sino que también escribirá todos los sumandos utilizados: 3 + 4 + 3 + 3 = 13.
+# El programa no se limitará a escribir el resultado dela suma, sino también escribirá
+# los sumandos utilizados: 3 + 4 + 3 + 3 = 13.
 
-Entrada
+# Entrada
 
-La entrada consta de una serie de casos de prueba terminados con un número negativo.
-Cada caso de prueba es una simple línea con un número no negativo no mayor que 109
-del que habrá que sumar todos sus dígitos.
+# La entrada consta de una serie de casos de prueba terminados con un número negativo.
+# Cada caso de prueba es una simple línea con un número no negativo no mayor que 10⁹
+# del que habrá que sumar todos sus dígitos.
 
-Salida
+# Salida
 
-Para cada caso de prueba se mostrará una línea en la que aparecerán cada uno de los dígitos
-separados por el signo +, tras lo cual aparecerá el símbolo = y la suma de todos los dígitos.
+# Para cada caso de prueba se mostrará una línea en la que aparecerán cada uno de los dígitos
+# separados por el signo +, tras lo cual aparecerá el símbolo = y la suma de todos los dígitos.
 
-Ten en cuenta que antes y después de cada símbolo(+ y=) hay un espacio.
+# Ten en cuenta que antes y después de cada símbolo(+ e =) hay un espacio.
 
-Nota: el programa no debe avisar al usuario con mensajes como "Introduzca un número".
-Debe leer directamente el número que introduzca el usuario y la respuesta debe ser exactamente
-como aparece aquí explicado y como se puede comprobar en el ejemplo.
-(Asegúrate de que has leído el resto de preguntas frecuentes antes de plantear tu solución)
+# Nota: el programa no debe avisar al usuario con mensajes como "Introduzca un número".
+# Debe leer directamente el número que introduzca el usuario y la respuesta debe ser exactamente
+# como aparece aquí explicado y como se puede comprobar en el ejemplo.
+# http://www.nachocabanes.com/retos/reto.php?n=024
+
+numero = 34331
+numeroStr = str(numero)
+resultado = ''
+suma = 0
+for pos in range(len(numeroStr)):
+    suma += int(numeroStr[pos])
+    if pos < (len(numeroStr)-1):
+        resultado += f'{numeroStr[pos]} + '
+    else:
+        resultado += numeroStr[pos]
+
+
+print(f'{resultado} = {suma}')
